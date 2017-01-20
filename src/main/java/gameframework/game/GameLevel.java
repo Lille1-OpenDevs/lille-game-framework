aa161b2e91735442a1b26b3a1fa638b18d342662
Joël Troch <joel.troch62@gmail.com>
2017-01-20 14:25:40 +0100

    Update documentation for GameLevel

--- src/main/java/gameframework/game/GameLevel.java
+++ src/main/java/gameframework/game/GameLevel.java
@@ -1,7 +1,14 @@
 package gameframework.game;
 
+/**
+ * Interface for game levels.
+ *
+ * Technically speaking, a "GameLevel" is just a thread whose initialize/simulate/free the level.
+ */
 public interface GameLevel extends Runnable {
+	/** Start the level (aka thread). */
 	public void start();
 
+	/** Stop the level (aka thread). */
 	public void end();
 }
