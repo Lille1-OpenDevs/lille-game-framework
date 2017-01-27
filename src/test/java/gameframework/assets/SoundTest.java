6d4f5409c8317a33486246152be8425ab12ab626
El Mehdi <mehdi.elhouti@etudiant.univ-lille1.fr>
2017-01-27 16:14:32 +0100

    add method to crossFading in Sound class

---
+++ src/test/java/gameframework/assets/SoundTest.java
@@ -0,0 +1,21 @@
+package gameframework.assets;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
+import org.junit.Test;
+
+public class SoundTest {
+
+	@Test
+	public void testcrossFading() throws UnsupportedAudioFileException, IOException, LineUnavailableException {
+		Sound soundTest = new Sound("/welcome.wav");
+		soundTest.crossFading(3000, 100);
+		assertFalse(soundTest.isPlaying());
+	}
+
+}
